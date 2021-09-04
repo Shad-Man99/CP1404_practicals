2eb43adb53369d765e845d785628b317950817c3
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2021-09-05 01:31:29 +1000

    Test

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,12 +1,26 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3:4]
-5 in numbers
-7 in numbers
-"3" in numbers
-numbers + [6, 5, 3]
+# 3
+# 2
+# 4
+# 3 1 4 1 5 9 2
+# 1 5
+# 5
+# 2
+# Error
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+print(numbers[2:7])
+
+for numbers.find("9")
+    print("9 is an element of numbers")
+    else print("9 is not an element of numbers")
+
+    # Possibly use in function, come back !!
+
+    if 9 in numbers
+print("9 is an element of numbers")
+else print("9 is not an element of numbers")
+
+numbers[0] = 'ten'
+numbers[2] = 1
