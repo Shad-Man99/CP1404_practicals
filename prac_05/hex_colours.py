fbf337e741d2ec508b19a09776e8f429b6b17ae7
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2021-09-05 23:30:55 +1000

    Practical 5

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+COLOUR_CODE = {"0000ff": "Blue", "ee3b3b": "Red", "eeeee0": "Ivory",
+               "7fff00": "Green",
+               "ff7f00": "Orange", "bf3eff": "Purple", "ee1289": "Pink", "cdad00": "Gold", "030303": "Black",
+               "8b1c62": "Maroon"}
+
+
+enter_code = input("Enter colour: ").upper()
+
+colour_output = {}
+for colour in COLOUR_CODE:
+    print()
+else:
+    print()
+
