cd19d3d7e0e0d6c3dcdc7b280384e93be984c52e
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2021-09-21 14:53:31 +1000

    Fix previous errors/mistakes

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,16 +1,15 @@
-class Guitar
+class Guitar:
 
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
 
-def __init__(self, name="", year=0, cost=0):
-    self.name = name
-    self.year = year
-    self.cost = cost
+    def __str__(self):
+        return "{}, ({}): ${}".format(self.name, self.year, self.cost)
 
+    def get_age(self):
+        pass
 
-def __str__(self):
-    return "{}, ({}): ${}".format(self.name, self.year, self.cost)
-
-def get_age
-
-def is_vintage
-
+    def is_vintage(self):
+        pass
