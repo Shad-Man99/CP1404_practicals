4779e0bd94dadfd6e6c34b93934a5a62dd07ad33
Shadman Ahmed <shadman.ahmed@my.jcu.edu.au>
2021-09-26 18:50:18 +1000

    Modify the existing GUI program for a boxlayout

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -9,4 +9,27 @@ class BoxLayoutDemo(App):
         return self.root
 
 
-BoxLayoutDemo().run()
+class ButtonEventDemo(App):
+    def handle_greet(self):
+        self.title = "Greet"
+        self.root.ids.output_label.text = " Hello " + self.root.ids.input_name.text
+        return self.root
+
+
+class ButtonEventDemo(App):
+    def output_label(self):
+        self.title = ''
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+
+class ButtonEventDemo(App):
+    def reset_label(self):
+        self.tile = "Clear text and output"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+        def press_button(self):
+        print("Greet")
+
+    BoxLayoutDemo().run()
